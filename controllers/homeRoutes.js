019ea9d9674b869273fb9fe8d78eca88afe0d0fd
Armando Ugalde <uarmando15@yahoo.com>
2022-07-05 18:47:58 -0700

    began work to add logic for controllers

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,10 +1,25 @@
 //These are all the view routes for your application
 const router = require('express').Router();
+const { User } = require('../models/User');
 
 //when a GET request is received on the root(/) route,
 //render the home.handlebars view
-router.get('/', (req, res) => {
-  res.render('home');
+router.get('/', async (req, res) => {
+ try {
+  const UserData = await User.create({
+    username: req.body.username,
+    password: req.body.password,
+    email: req.body.email,
+ });
+ req.session.save(() => {
+  req.session.loggedIn = true;
+  req.status(200).json(UserData);
+ });
+}
+catch (err) {
+  console.log(err);
+  res.status(500).json(err);
+}
 });
 
 router.get('/otherpage', (req, res) => {

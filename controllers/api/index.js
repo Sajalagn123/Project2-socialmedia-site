21d19f0e1d795f458e88d444f86d505723a78ce0
Armando Ugalde <uarmando15@yahoo.com>
2022-07-05 19:34:22 -0700

    changed names of consts used in api index file

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,7 @@
 //THIS index file is the entry point of our API(data) routes,
 //it will bring in all api routes and export 1 router middleware
 const router = require('express').Router();
-const exampleRoutes = require('./exampleAPIRoutes');
-router.use('/example', exampleRoutes);
+const userRoutes = require('./userRoutes');
+router.use('/users', userRoutes);
 
 module.exports = router;
